baa0669abe1d57ef513db75b1b3a486a5e6a262a
DhruvPasricha <78498002+DhruvPasricha@users.noreply.github.com>
2021-07-27 01:53:20 +0530

    overload << >> = operators

--- bigint.h
+++ bigint.h
@@ -4,8 +4,8 @@ using namespace std;
 class bigint
 {
 
-    // data members [start]
 private:
+    // data members [start]
     string num;
     // data members [end]
 
@@ -15,19 +15,42 @@ public:
     {
         this->num = "";
     }
-    bigint(string num) // copy constructor
+    bigint(const bigint &b) // copy constructor
+    {
+        this->num = b.num;
+    }
+    bigint(string num) // parameterised constructor
     {
         this->num = num;
     }
     // constructors [end]
 
     // input/output operators (>> <<) [start]
+    friend istream &operator>>(istream &input, bigint &b)
+    {
+        input >> b.num;
+        return input;
+    }
+    friend ostream &operator<<(ostream &output, const bigint &b)
+    {
+        output << b.num;
+        return output;
+    }
     // input/output operators (>> <<) [end]
-    
-    
+
+    // assignment operator [start]
+    void operator=(const bigint &b)
+    {
+        this->num = b.num;
+    }
+    void operator=(const string &num)
+    {
+        this->num = num;
+    }
+    // assignment operator [end]
+
     // arithematic operators (+ - * / %) [start]
     // arithematic operators (+ - * / %) [end]
-
 
     // Math functions (gcd/lcm fibonacci factorial)[start]
     // Math functions (gcd/lcm fibonacci factorial)[start]
