878ca915aa27a79ec522dd623611300f46343a91
DhruvPasricha <78498002+DhruvPasricha@users.noreply.github.com>
2021-07-27 01:19:04 +0530

    add data member and constructors

---
+++ bigint.h
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class bigint
+{
+
+    // data members [start]
+private:
+    string num;
+    // data members [end]
+
+public:
+    // constructors [start]
+    bigint() // default constructor
+    {
+        this->num = "";
+    }
+    bigint(string num) // copy constructor
+    {
+        this->num = num;
+    }
+    // constructors [end]
+
+    // input/output operators (>> <<) [start]
+    // input/output operators (>> <<) [end]
+    
+    
+    // arithematic operators (+ - * / %) [start]
+    // arithematic operators (+ - * / %) [end]
+
+
+    // Math functions (gcd/lcm fibonacci factorial)[start]
+    // Math functions (gcd/lcm fibonacci factorial)[start]
+};
