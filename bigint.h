eefa543bedb16fd518acd519ce56ab56bd4dce49
DhruvPasricha <78498002+DhruvPasricha@users.noreply.github.com>
2021-07-27 02:25:27 +0530

    overload + operator

--- bigint.h
+++ bigint.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 class bigint
@@ -50,8 +51,79 @@ public:
     // assignment operator [end]
 
     // arithematic operators (+ - * / %) [start]
+    bigint operator+(const bigint &b)
+    {
+        bigint result;
+        result.num = add(this->num, b.num);
+        return result;
+    }
+    bigint operator-(const bigint &b)
+    {
+        bigint result;
+        result.num = subtract(this->num, b.num);
+        return result;
+    }
+    bigint operator*(const bigint &b)
+    {
+        bigint result;
+        result.num = multiply(this->num, b.num);
+        return result;
+    }
+    bigint operator/(const bigint &b)
+    {
+        bigint result;
+        result.num = divide(this->num, b.num);
+        return result;
+    }
+    bigint operator%(const bigint &b)
+    {
+        bigint result;
+        result.num = modulo(this->num, b.num);
+        return result;
+    }
     // arithematic operators (+ - * / %) [end]
 
     // Math functions (gcd/lcm fibonacci factorial)[start]
     // Math functions (gcd/lcm fibonacci factorial)[start]
+
+private:
+    // arithematic operations performed using string [start]
+    string add(const string &a, const string &b)
+    {
+        string res = "";
+        int length1 = a.length();
+        int length2 = b.length();
+        int carry = 0;
+
+        while (length1 > 0 or length2 > 0 or carry > 0)
+        {
+            int currentDigit1 = (length1 > 0) ? a[length1 - 1] - '0' : 0;
+            int currentDigit2 = (length2 > 0) ? b[length2 - 1] - '0' : 0;
+
+            int currentDigit = currentDigit1 + currentDigit2 + carry;
+
+            carry = currentDigit / 10;
+            currentDigit %= 10;
+
+            res += (char)(currentDigit + '0');
+
+            length1--;
+            length2--;
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+    string subtract(const string &a, const string &b)
+    {
+    }
+    string multiply(const string &a, const string &b)
+    {
+    }
+    string divide(const string &a, const string &b)
+    {
+    }
+    string modulo(const string &a, const string &b)
+    {
+    }
+    // arithematic operations performed using string [end]
 };
